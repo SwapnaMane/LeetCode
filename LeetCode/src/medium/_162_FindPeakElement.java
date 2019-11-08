45a1b5f010d4ba82b11053d754d1779ac395954a
swapn <swapn@LAPTOP-04UVCMS7>
2019-11-08 12:12:24 -0800

    dfg

--- LeetCode/src/medium/_162_FindPeakElement.java
+++ LeetCode/src/medium/_162_FindPeakElement.java
@@ -10,7 +10,7 @@ public class _162_FindPeakElement {
 					 return 0;
 				 else
 					 return 1;
-			 }
+			 } 
 	        int index = 0;
 	         for(int i =1 ; i< nums.length ; i++){
 		    	 if(nums[i-1]<nums[i] ){
